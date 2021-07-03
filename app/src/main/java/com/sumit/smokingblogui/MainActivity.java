e847dd66d678d9fd8e306ea21984c6194d18ca7f
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-07-03 09:06:55 +0530

    Blog Ui

---
+++ app/src/main/java/com/sumit/smokingblogui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sumit.smokingblogui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
